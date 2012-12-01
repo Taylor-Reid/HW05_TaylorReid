4aced5a25e6b50879948eadd5d820376413f0cce
Taylor Reid <reidta3@muohio.edu>
2012-12-01 15:54:36 -0500

    Matrix & List ( partially working
    Matrix passes tests 1-8
    List passes tests 1 & 2, crashes on 10
    Unsure why anything is failing/crashing

--- HW05_TaylorReid/ListGraph.cpp
+++ HW05_TaylorReid/ListGraph.cpp
@@ -16,7 +16,21 @@ ListGraph::~ListGraph(){
    *     weight > 0
    */
    void ListGraph::addEdge(NodeID u, NodeID v, EdgeWeight weight) {
-	  //don't forget to num_edges++;
+		EList::const_iterator it;
+		NWPair check;
+		if(u>=0 && u < edgeList.size() && v>=0 && v < edgeList.size() && u!=v){
+			for(it = edgeList[u].begin(); it != edgeList[u].end();it++){
+				check =*it;
+				if(check.first == v ){//if an edge already exists, we do nothing and exit
+					return;
+				}
+			}//if the for loop found no edge, we make one
+			if(weight>0){
+				edgeList[u].push_back(NWPair(v,weight));
+				edgeList[v].push_back(NWPair(u,weight));
+				num_edges++;
+			}
+		}
   }
   
 
@@ -27,7 +41,18 @@ ListGraph::~ListGraph(){
    *     u and v are legal labels (i.e. 0 <= u < G.size(), 0 <= v < G.size())
    */
   EdgeWeight ListGraph::weight(NodeID u, NodeID v) const {
-	  return 0.0;
+		EList::const_iterator it;
+		NWPair check;
+		if(u>=0 && u < edgeList.size() && v>=0 && v < edgeList.size() && u!=v){
+			for(it = edgeList[u].begin(); it != edgeList[u].end();it++){
+				check = *it;
+				if(check.first == v ){
+					return check.second;
+				}
+			}
+			
+		}
+	  return EdgeWeight(0);
   }
 
   /*
@@ -35,10 +60,8 @@ ListGraph::~ListGraph(){
    *
    * Preconditions: u is a legal label.
    */
-  std::list<NWPair> ListGraph::getAdj(NodeID u) const {
-	  std::list<NWPair> aList;
-	  
-	  return aList;
+  std::list<NWPair> ListGraph::getAdj(NodeID u) const {  
+	  return edgeList[u];
   }
 
   /*
@@ -47,14 +70,16 @@ ListGraph::~ListGraph(){
    * Preconditions: u is a legal label;
    */
   unsigned ListGraph::degree(NodeID u) const {
-	  
+	  return edgeList[u].size();
+	  //not sure this is right, need to check...
+
   }
 
   /*
    * Return the number of nodes in the graph.
    */
   unsigned ListGraph::size() const {
-	  return 0.0;
+	  return edgeList.size();
   }
 
   /* 
